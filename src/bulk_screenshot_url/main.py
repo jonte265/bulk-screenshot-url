36ea3265cd0c9290feb1bdb3f3840c7d7a252fc7
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-08-27 10:26:34 +0200

    add screenshot every line from sites.txt

--- src/bulk_screenshot_url/main.py
+++ src/bulk_screenshot_url/main.py
@@ -3,11 +3,22 @@ from playwright.sync_api import sync_playwright
 
 def main() -> None:
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("https://playwright.dev")
-        page.screenshot(path="screenshots/example.png")
-        # print(page.title())
-        print("Done")
-        browser.close()
+    with open(
+        "sites/sites.txt",
+        "r",
+    ) as file:
+        for line in file:
+            site = line.strip()
+            print(site)
+
+            with sync_playwright() as p:
+                browser = p.chromium.launch()
+                page = browser.new_page()
+                page.goto(site)
+                print("Taking screenshot: ", site)
+                page.screenshot(
+                    path=f"screenshots/{site.removeprefix('https://').replace('/', '').replace('.', '').replace('www', '')}.png"
+                )
+                # print(page.title())
+                print("Done")
+                browser.close()
