082ac68ba89ba6b4d4c2b12dfff69fa7afade287
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-08-27 11:34:34 +0200

    add mobile viewport flag and improve loop screenshot faster

--- src/bulk_screenshot_url/main.py
+++ src/bulk_screenshot_url/main.py
@@ -1,24 +1,38 @@
-from playwright.sync_api import sync_playwright
+import typer
+from playwright.sync_api import ViewportSize, sync_playwright
+
+
+def run(mobile: bool = False) -> None:
+
+    chosen = "desktop"
+
+    if mobile:
+        chosen = "mobile"
+
+    viewports: dict[str, ViewportSize] = {
+        "desktop": {"width": 1280, "height": 720},
+        "mobile": {"width": 390, "height": 844},
+    }
+
+    with (
+        open(
+            "sites/sites.txt",
+            "r",
+        ) as file,
+        sync_playwright() as p,
+    ):
+        browser = p.chromium.launch()
+        page = browser.new_page(viewport=viewports[chosen])
+        for line in file:
+            site = line.strip()
+            page.goto(site)
+            print("Taking screenshot:", site)
+            page.screenshot(
+                path=f"screenshots/{site.removeprefix('https://').replace('/', '').replace('.', '').replace('www', '')}.png"
+            )
+            print("Done")
+        browser.close()
 
 
 def main() -> None:
-
-    with open(
-        "sites/sites.txt",
-        "r",
-    ) as file:
-        for line in file:
-            site = line.strip()
-            print(site)
-
-            with sync_playwright() as p:
-                browser = p.chromium.launch()
-                page = browser.new_page()
-                page.goto(site)
-                print("Taking screenshot: ", site)
-                page.screenshot(
-                    path=f"screenshots/{site.removeprefix('https://').replace('/', '').replace('.', '').replace('www', '')}.png"
-                )
-                # print(page.title())
-                print("Done")
-                browser.close()
+    typer.run(run)
