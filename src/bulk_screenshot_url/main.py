ffc7b5b3afb7355dc6c28a903a37fb964b1fca19
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-08-27 12:00:43 +0200

    add tablet option

--- src/bulk_screenshot_url/main.py
+++ src/bulk_screenshot_url/main.py
@@ -1,16 +1,16 @@
+from typing import Literal
+
 import typer
 from playwright.sync_api import ViewportSize, sync_playwright
 
 
-def run(mobile: bool = False, delay_ms: int = 0) -> None:
-
-    chosen = "desktop"
-
-    if mobile:
-        chosen = "mobile"
+def run(
+    device: Literal["desktop", "mobile", "tablet"] = "desktop", delay_ms: int = 0
+) -> None:
 
     viewports: dict[str, ViewportSize] = {
         "desktop": {"width": 1280, "height": 720},
+        "tablet": {"width": 768, "height": 1024},
         "mobile": {"width": 390, "height": 844},
     }
 
@@ -22,14 +22,14 @@ def run(mobile: bool = False, delay_ms: int = 0) -> None:
         sync_playwright() as p,
     ):
         browser = p.chromium.launch()
-        page = browser.new_page(viewport=viewports[chosen])
+        page = browser.new_page(viewport=viewports[device])
         for line in file:
             site = line.strip()
             page.goto(site)
-            print("Taking screenshot:", site)
+            print(f"Taking screenshot: {site} ({device}) (delay-ms: {delay_ms})")
             page.wait_for_timeout(delay_ms)
             page.screenshot(
-                path=f"screenshots/{site.removeprefix('https://').replace('/', '').replace('.', '').replace('www', '')}_{chosen}.png"
+                path=f"screenshots/{site.removeprefix('https://').replace('/', '').replace('.', '').replace('www', '')}_{device}.png"
             )
             print("Done")
         browser.close()
