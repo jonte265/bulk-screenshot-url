63ba559a2af444238f8efa75ad64a66232b3e67e
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-08-27 09:36:27 +0200

    screenshot page

--- src/bulk_screenshot_url/main.py
+++ src/bulk_screenshot_url/main.py
@@ -1,6 +1,13 @@
-def main() -> None:
-    print("Hello from bulk-screenshot-url!")
+from playwright.sync_api import sync_playwright
 
-    print("Hello")
-    print("Hello")
-    print("Hello")
+
+def main() -> None:
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("https://playwright.dev")
+        page.screenshot(path="screenshots/example.png")
+        # print(page.title())
+        print("Done")
+        browser.close()
